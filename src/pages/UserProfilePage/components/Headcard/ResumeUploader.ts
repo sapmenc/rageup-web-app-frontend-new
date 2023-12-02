5f3fdc677c2aab7c1ff808860ba928f29450c308
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2023-12-03 01:01:00 +0530

    added resume uploader comp

---
+++ src/pages/UserProfilePage/components/Headcard/ResumeUploader.ts
@@ -0,0 +1,56 @@
+import React from "react";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import useMediaUploader from "../../../../hooks/useMediaUploader";
+
+const ResumeUploader = () => {
+  const {
+    getRootProps,
+    getInputProps,
+    file,
+    uploadedUrl,
+    isUploading,
+    uploadFile,
+    copyToClipboard,
+    resetUploader,
+  } = useMediaUploader();
+
+  return (
+    <Box maxW="md" mx="auto" mt={8} p={4} borderWidth="1px" borderRadius="lg">
+      <Heading mb={4} textAlign="center">
+        Resume Uploader
+      </Heading>
+
+      <div {...getRootProps()} style={{ textAlign: "center" }}>
+        <input {...getInputProps()} />
+        <p>{file ? `Selected File: ${file.name}` : "Drag 'n' drop your resume file here, or click to select a file"}</p>
+      </div>
+
+      <Button
+        colorScheme="teal"
+        mt={4}
+        isLoading={isUploading}
+        loadingText="Uploading..."
+        onClick={uploadFile}
+        isDisabled={!file || isUploading}
+      >
+        Upload Resume
+      </Button>
+
+      {uploadedUrl && (
+        <Box mt={4}>
+          <Text>Uploaded URL:</Text>
+          <Text>{uploadedUrl}</Text>
+          <Button mt={2} onClick={copyToClipboard}>
+            Copy to Clipboard
+          </Button>
+        </Box>
+      )}
+
+      <Button mt={4} onClick={resetUploader} isDisabled={!uploadedUrl}>
+        Reset
+      </Button>
+    </Box>
+  );
+};
+
+export default ResumeUploader;
