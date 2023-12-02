ff35830337e1474606f1df5787332f78982b011a
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2023-12-02 15:03:06 +0530

    added google oauth wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <GoogleOAuthProvider clientId="120996457334-0n45rvqgrb4mdjspd9jc2fef693rqqbk.apps.googleusercontent.com">
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
