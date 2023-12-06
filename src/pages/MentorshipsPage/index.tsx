099c1ebc33ed9a8459955c174e726c5b36d4ffc4
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2023-12-07 01:35:54 +0530

    added the mentor profile picture

--- src/pages/MentorshipsPage/index.tsx
+++ src/pages/MentorshipsPage/index.tsx
@@ -95,7 +95,7 @@ const MentorshipsPage = () => {
                       <MentorCard
                         mentorId={mentor._id}
                         mentorName={mentor?.mentorName || ""}
-                        image="https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"
+                        image={mentor?.profilePicture || ""}
                         strengths={mentor?.strengths || ""}
                       />
                     </GridItem>
