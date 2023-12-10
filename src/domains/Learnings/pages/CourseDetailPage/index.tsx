63b4128a0a3fa2435e341dbc0cf8a6027ccbbaab
saranshkhulbe7 <saranshkhulbe7@gmail.com>
2023-12-10 13:07:36 +0530

    fix

--- src/domains/Learnings/pages/CourseDetailPage/index.tsx
+++ src/domains/Learnings/pages/CourseDetailPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import PageLayout from "../../../../layouts/PageLayout";
 import FieldCheckLayout from "../../../../layouts/FieldCheckLayout";
 import { Box, Flex, Heading } from "@chakra-ui/react";
@@ -22,14 +22,15 @@ const CourseDetailPage = () => {
           <Flex justifyContent={"center"}>
             <Flex
               flexDirection={"column"}
-              py={2}
+              pt={10}
+              pb={2}
               px={4}
               gap={4}
               w={"full"}
               maxW={"2xl"}
             >
               {/* Course Heading */}
-              <Heading>Course Heading</Heading>
+              <Heading fontSize={"2xl"}>Course Heading</Heading>
               <Flex flexDirection={"column"} gap={3}>
                 {[1, 1, 1, 1, 1, 1].map((_, key) => {
                   return (
